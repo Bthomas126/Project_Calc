8506b58c6f5d9a71b87f65ac59132a3a468ed46b
Blake Thomas <bthomas6012@floridapoly.edu>
2026-06-05 15:49:12 +0000

    save 1

---
+++ main.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+
+template <typename T>
+class Stack{
+
+    private:
+        T arr[100];
+        int top;
+
+    public:
+        Stack(){
+
+            top = -1;
+
+        }
+        
+        void push(T x){
+            if(top >= 99){
+                return;
+            }
+            
+            top++;
+            arr[top] = x;
+
+        }
+       
+        T pop(){
+            if(top ==  -1){
+                return T();
+            }
+
+            top--;
+            return arr[top + 1];
+        }
+       
+        T peek(){
+            if(top != -1){
+
+            return arr[top];
+            
+            }
+        }
+        
+        bool isEmpty(){
+
+            if(top == -1){
+
+                return true;
+
+            }
+
+            return false;
+            
+
+        }
+
+};
+
+
+
+
+
+
+
+
+
+int main(){
+
+
+
+    return 0;
+}
